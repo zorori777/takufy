5193e86147d8e153d8caf84e7241c0a1a702d91b
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-04-14 20:17:34 +0900

    Implement functions  edit and update

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,34 @@
 class UserController < ApplicationController
 
+before_action :setting_user
+
   #Makimg user instance
   def show
+  end
+
+  # Edit page
+  def edit
+  end
+
+  # Update user information
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "編集成功しました"
+    else
+      flash.now[:alert] = "編集は失敗しました"
+      render :edit
+    end
+  end
+
+private
+
+  # Setting strong parameter
+  def user_params
+    params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana)
+  end
+
+  # Helper method for common instances
+  def setting_user
     @user = User.find(params[:id])
   end
 
