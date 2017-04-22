08f8e1cf9eaf4c38ecfdb3016bfe60c39705b29f
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-04-22 19:19:04 +0900

    Making edit and update function

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,7 @@
 class AnswersController < ApplicationController
 
   before_action :redirect_page, only: :new
+  before_action :respond_answer, only: [:edit, :update]
 
   def new
     @question = Question.find(params[:question_id])
@@ -17,13 +18,36 @@ class AnswersController < ApplicationController
         flash.now[:alert] = "解答作成できませんでした"
         render :new
       end
-
   end
+
+  def edit
+  end
+
+  def update
+    if @answer.update(update_params)
+      redirect_to root_path, notice: "編集がしました"
+    else
+      flas.now[:alert] ="編集できませんでした"
+      render :edit
+    end
+  end
+
 
 # Settiong strong parameters
 private
   def making_answer
     params.require(:answer).permit(:text, :question_id)
+  end
+
+# making heplet method
+  def respond_answer
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+  end
+
+ #setting strong parameter
+   def update_params
+    params.require(:answer).permit(:text)
   end
 
   # Avoid answering same question again
